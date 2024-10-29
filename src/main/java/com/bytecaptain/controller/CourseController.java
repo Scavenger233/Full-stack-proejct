c6f3850ebcbf9173a38b9007a27dac574adcfd27
Scavenger233 <liyiminginireland@gmail.com>
2024-10-29 12:10:27 +0000

    Connect backend with frontend by add @CrossOrigin annotation

--- src/main/java/com/bytecaptain/controller/CourseController.java
+++ src/main/java/com/bytecaptain/controller/CourseController.java
@@ -8,19 +8,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bytecaptain.model.Course;
 import com.bytecaptain.service.CourseService;
 
-//@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 public class CourseController {
 	
